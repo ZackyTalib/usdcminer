d8dbeeaa9c4ad3eb613f1bac1352439aad0709c5
ZackyTalib <38492137+ZackyTalib@users.noreply.github.com>
2022-02-11 14:44:01 +0800

    fgdfgsdg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import Web3 from "web3";
 
 function Header(props: { walletConnected: boolean, connectWallet: () => void }) {
   return (
@@ -17,16 +18,12 @@ function Hero(props: { walletConnected: boolean, connectWallet: () => void }) {
   return (
     <div className="mt-16 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-20 md:mr-20 lg:ml-24 lg:mr-24 xl:ml-28 xl:mr-28 flex flex-col-reverse lg:flex-row">
       <div className="w-full lg:w-1/2 pr-8 mt-16 lg:mt-0">
-        <h3 className="mb-2.5 tracking-wider">SAFEST MINING GAME ON FANTOM</h3>
+        <h3 className="mb-2.5 tracking-wider font-light">SAFEST MINING GAME ON FANTOM</h3>
         <h1 className="font-bold text-5xl sm:text-6xl">Start Earning More <span className="text-blue-500">USDC</span></h1>
         <p className="mt-10 leading-7">
           You can earn extra money on your USDC by purchasing our miners. Our miners will give you a 1% ROI, which can be compounded to generate even higher returns. USDCMINER is the safest mining game on Fantom. Connect your wallet and start earning now!
         </p>
-        <div className={props.walletConnected ? "hidden" : "" + " flex flex-col mt-10 mr-0 sm:mr-10"}>
-          <label className="text-sm" htmlFor="refferal">Have a refferal link?</label>
-          <input className="mt-2.5 text-sm p-3 border border-gray-300 rounded-md" type="text" name="refferal" placeholder="Enter refferal address..." />
-        </div>
-        <button onClick={props.walletConnected ? () => window.location.href = "#dashboard" : () => props.connectWallet()} className={(props.walletConnected ? "mt-16" : "mt-10") + " w-fit transition-all hover:scale-105 pl-6 pr-6 rounded-lg bg-blue-500 text-white font-bold p-4"}>{props.walletConnected ? "Goto Dashboard" : "Connect Wallet"}</button>
+        <button onClick={props.walletConnected ? () => window.location.href = "#dashboard" : () => props.connectWallet()} className="mt-16 w-fit transition-all hover:scale-105 pl-6 pr-6 rounded-lg bg-blue-500 text-white font-bold p-4">{props.walletConnected ? "Goto Dashboard" : "Connect Wallet"}</button>
       </div>
       <img src="/images/hero.svg" className="w-full lg:w-1/2" />
     </div>
@@ -58,25 +55,34 @@ function LinkCards() {
   )
 }
 
-function DashboardLeft() {
+function DashboardLeft(props: { minersCount: number, deposit: (usdcAmount: number, refferalAddress: string) => void }) {
+  let [usdcAmount, setUsdcAmount] = useState("");
+  let [refferalLink, setReferralLink] = useState("");
+  const formSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (usdcAmount == "") return;
+    props.deposit(parseInt(usdcAmount), refferalLink);
+  };
   return (
     <div className="md:w-1/2">
       <h3 className="font-bold text-2xl mb-8">Your Miners</h3>
-      <p>You have 0 miners</p>
-      <form className="mt-8 flex flex-col">
+      <p>You have <span className="font-semibold">{props.minersCount / (10 ** 18)}</span> miners</p>
+      <form className="mt-8 flex flex-col" onSubmit={formSubmit}>
         <label htmlFor="amount" className="text-sm mb-2.5">Enter USDC amount</label>
         <div className="flex items-center">
-          <input className="text-sm w-72 flex-grow p-3 rounded-md border border-gray-300" name="amount" type="text" placeholder="1" />
-          <p className="ml-4">USDC</p>
+          <input value={usdcAmount} onChange={(e) => setUsdcAmount(e.target.value)} className="text-sm flex-grow p-3 rounded-md border border-gray-300" name="amount" type="number" placeholder="USDC amount..." />
+          <p className="ml-4 font-semibold">USDC</p>
         </div>
-        <button className="pl-12 pr-12 max-w-none md:max-w-xs hover:scale-105 transition-all mt-8 self-start p-3 bg-blue-500 text-white font-bold text-sm rounded-md">Buy Miners</button>
+        <label htmlFor="refferal" className="text-sm mb-2.5 mt-6">Referral address (if any)</label>
+        <input value={refferalLink} onChange={(e) => setReferralLink(e.target.value)} className="text-sm flex-grow p-3 rounded-md border border-gray-300" name="refferal" type="text" placeholder="Refferral link..." />
+        <button className={(usdcAmount == "" ? "bg-gray-400 opacity-90 " : "hover:scale-105 bg-blue-500 ") + " pl-12 pr-12 max-w-none md:max-w-xs transition-all mt-8 self-start p-3 bg-blue-500 text-white font-bold text-sm rounded-md"}>Buy Miners</button>
       </form>
     </div>
   )
 }
 
 function Timer(props: { time: number, completed: () => void }) {
-  const [time, setTime] = useState(props.time);
+  const [time, setTime] = useState(Math.round(Date.now() / 1000) - props.time);
   const complete = () => {
     props.completed();
     return 0;
@@ -87,42 +93,87 @@ function Timer(props: { time: number, completed: () => void }) {
   }, []);
   return (
     <div className="text-center rounded-md mt-12 p-5 bg-red-400 text-white">
-      <p className="text-sm font-bold">Please wait before withdrawing/compounding again.</p>
-      <p className="font-bold mt-5">{String(Math.floor(time / 3600)).padStart(2, "0")} : {String(Math.floor((time % 3600) / 60)).padStart(2, "0")} : {String((time % 3600) % 60).padStart(2, "0")}</p>
+      <p className="text-sm">Please wait before withdrawing/compounding again.</p>
+      <p className="font-semibold mt-5">{String(Math.floor(time / 3600)).padStart(2, "0")} : {String(Math.floor((time % 3600) / 60)).padStart(2, "0")} : {String((time % 3600) % 60).padStart(2, "0")}</p>
     </div>
   )
 }
 
-function DashboardRight() {
-  const [countdown, setCountdown] = useState(true);
+function DashboardRight(props: { timestamp: number, minersCount: number, walletUSDC: number, compound: () => void, withdraw: () => void }) {
+  const [countdown, setCountdown] = useState(false);
+  useEffect(() => props.timestamp <= 0 ? setCountdown(false) : setCountdown(true), [props.timestamp]);
   return (
     <div className="md:w-1/2">
       <h3 className="font-bold text-2xl mb-8">Your USDC</h3>
-      <p>You have ? USDC in your barrel</p>
-      <p className="mt-1">You have ? USDC in your wallet</p>
+      <p>You have <span className="font-semibold">{!countdown ? ((props.minersCount / (10 ** 18)) / 100) : 0} USDC</span> in your barrel</p>
+      <p className="mt-1">You have <span className="font-semibold">{props.walletUSDC} USDC</span> in your wallet</p>
       {
-        countdown ?
-          <Timer time={20} completed={() => setCountdown(false)} />
+        countdown && props.timestamp > 0 ?
+          <Timer time={props.timestamp} completed={() => setCountdown(false)} />
           :
           <div className="w-56 flex flex-col mt-8 max-w-none md:max-w-xs">
-            <button className="w-full hover:scale-105 transition-all mt-4 self-start p-3 pl-0 pr-0 bg-blue-500 text-white font-bold text-sm rounded-md">Compound USDC</button>
-            <button className="w-full hover:scale-105 transition-all mt-4 self-start p-3 pl-0 pr-0 bg-blue-500 text-white font-bold text-sm rounded-md">Pocket USDC</button>
+            <button onClick={props.compound} className="w-full hover:scale-105 transition-all mt-4 self-start p-3 pl-0 pr-0 bg-blue-500 text-white font-bold text-sm rounded-md">Compound USDC</button>
+            <button onClick={props.withdraw} className="w-full hover:scale-105 transition-all mt-4 self-start p-3 pl-0 pr-0 bg-blue-500 text-white font-bold text-sm rounded-md">Withdraw USDC</button>
           </div>
       }
     </div>
   )
 }
 
-function Dashboard() {
+function Dashboard(props: { address: string }) {
+  let [contractBalance, setContractBalance] = useState(0);
+  let [minersCount, setMinersCount] = useState(0);
+  let [walletUSDC, setWalletUSDC] = useState(0);
+  let [timestamp, setTimestamp] = useState(0);
+
+  const updateContractBalance = () => {
+    //setContractBalance(Math.floor(Math.random() * (100 - 1 + 1) + 1))
+  };
+
+  const updateMinersCount = () => {
+    //setMinersCount(Math.floor(Math.random() * (10000000000000000000 - 10000000000000000 + 1) + 10000000000000000));
+  };
+
+  const updateWalletUSDC = () => {
+    //setWalletUSDC(Math.floor(Math.random() * (100 - 1 + 1) + 1))
+  };
+
+  const updateTimestamp = () => {
+    //setTimestamp(Math.round(Date.now() / 1000) - 10)
+  };
+
+  const deposit = (usdcAmount: number, referralAddress: string) => {
+    updateMinersCount();
+    updateContractBalance();
+    updateWalletUSDC();
+  }
+
+  const withdraw = () => {
+    updateContractBalance();
+    updateTimestamp();
+  }
+
+  const compound = () => {
+    updateMinersCount();
+    updateTimestamp();
+  }
+
+  useEffect(() => {
+    updateContractBalance();
+    updateMinersCount();
+    updateTimestamp();
+    updateWalletUSDC();
+  }, []);
+
   return (
     <div className="m-24 mb-0 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-20 md:mr-20 lg:ml-24 lg:mr-24 xl:ml-28 xl:mr-28" id="dashboard">
       <h2 className="font-bold text-4xl">Dashboard</h2>
-      <p className="mt-6">Contract balance: ?</p>
-      <p className="mt-1">Your referral address: ?</p>
+      <p className="mt-6">Contract balance: <span className="font-semibold">{contractBalance} USDC</span></p>
+      <p className="mt-1">Your referral address: <span className="font-semibold">{props.address}</span></p>
       <div className="mt-12 flex flex-col md:flex-row">
-        <DashboardLeft />
+        <DashboardLeft minersCount={minersCount} deposit={deposit} />
         <div className="border border-gray-300 ml-0 mr-0 md:mr-12 md:ml-12 lg:ml-20 lg:mr-20 mt-10 mb-10 md:mt-0 md:mb-0"></div>
-        <DashboardRight />
+        <DashboardRight timestamp={timestamp} minersCount={minersCount} walletUSDC={walletUSDC} withdraw={withdraw} compound={compound} />
       </div>
     </div>
   )
@@ -184,7 +235,7 @@ function Footer(props: { walletConnected: boolean, connectWallet: () => void }) 
         <div className="flex items-center">
           <h3 className="font-bold">USDCMINER</h3>
         </div>
-        <p className="mt-2 text-xs tracking-wider">SAFEST MINING GAME ON PHANTOM</p>
+        <p className="mt-2 text-xs tracking-wider font-light">SAFEST MINING GAME ON PHANTOM</p>
         <p className="text-sm mt-12">Copyright © 2022 USDCMINER</p>
       </header>
       <FooterLinks />
@@ -193,50 +244,55 @@ function Footer(props: { walletConnected: boolean, connectWallet: () => void }) 
   )
 }
 
-interface AccountProps {
-  contractBalance: number,
-  referralAddress: string,
-  minersAmount: number,
-  barrelUSDC: number,
-  walletUSDC: number
-}
+declare const window: any;
 
 export default function Home() {
   let [walletConnect, setWalletConnect] = useState(false);
-  const connectWallet = () => {
-    setWalletConnect(true);
-  };
+  let [address, setAddress] = useState("");
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      window.web3 = new Web3(window.ethereum);
+      setWalletConnect(true);
+    }
+  }
+
   return (
     <div>
       <Head>
         <title>USDCMINER</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="title" content="USDC Miner - Earn More USDC" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="You can earn extra money on your USDC by purchasing our miners. Our miners will give you a 1% ROI, which can be compounded to generate even higher returns. USDCMINER is the safest mining game on Fantom. Connect your wallet and start earning now!" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://usdcminer.vercel.app" />
         <meta property="og:title" content="USDC Miner - Earn More USDC" />
         <meta property="og:description" content="You can earn extra money on your USDC by purchasing our miners. Our miners will give you a 1% ROI, which can be compounded to generate even higher returns. USDCMINER is the safest mining game on Fantom. Connect your wallet and start earning now!" />
         <meta property="og:image" content="/images/screenshot.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://usdcminer.vercel.app" />
         <meta property="twitter:title" content="USDC Miner - Earn More USDC" />
         <meta property="twitter:description" content="You can earn extra money on your USDC by purchasing our miners. Our miners will give you a 1% ROI, which can be compounded to generate even higher returns. USDCMINER is the safest mining game on Fantom. Connect your wallet and start earning now!" />
         <meta property="twitter:image" content="/images/screenshot.png" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet" />
       </Head>
       <div>
-        <Header walletConnected={walletConnect} connectWallet={connectWallet} />
+        <Header walletConnected={walletConnect} connectWallet={() => connectWallet()} />
         <main>
-          <Hero walletConnected={walletConnect} connectWallet={connectWallet} />
+          <Hero walletConnected={walletConnect} connectWallet={() => connectWallet()} />
           <LinkCards />
           {
-            walletConnect ? <Dashboard /> : <UnconnectedDashboard connectWallet={connectWallet} />
+            walletConnect ? <Dashboard address={address} /> : <UnconnectedDashboard connectWallet={() => connectWallet()} />
           }
         </main>
-        <Footer walletConnected={walletConnect} connectWallet={connectWallet} />
+        <Footer walletConnected={walletConnect} connectWallet={() => connectWallet()} />
       </div>
     </div>
   )
